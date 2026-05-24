31c5a64933bb32d56adcf9dc67fa86ce7d6c48b5
seelamsrivani2004-max <novahamotech75@gmail.com>
2026-05-24 17:45:13 +0530

    fixed cors completely

--- backend/hr_management/settings.py
+++ backend/hr_management/settings.py
@@ -19,11 +19,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'corsheaders',
+
     'api',
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,4 +68,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://novahamohrmanagementsystem.netlify.app",
+]
