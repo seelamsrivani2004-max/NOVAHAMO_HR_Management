e103156d96575d8a8f73e7ab262501e30ce00615
seelamsrivani2004-max <novahamotech75@gmail.com>
2026-05-24 18:33:01 +0530

    fixed cors completely

--- backend/hr_management/settings.py
+++ backend/hr_management/settings.py
@@ -69,6 +69,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
